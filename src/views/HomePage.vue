6a51cc875c94a59e6151250fab49429a20d8ef5d
rojsiroj <sirojudin.dev@gmail.com>
2024-11-27 21:17:25 +0700

    finishing and make sure same with example

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -20,10 +20,14 @@
           </ion-row>
 
           <!-- Table Rows -->
-          <ion-row class="table-row">
-            <ion-col>Bitcoin</ion-col>
-            <ion-col>BTC</ion-col>
-            <ion-col>50,000</ion-col>
+          <ion-row
+            v-for="crypto in cryptoData"
+            :key="crypto.id"
+            class="table-row"
+          >
+            <ion-col>{{ crypto.name }}</ion-col>
+            <ion-col>{{ crypto.symbol }}</ion-col>
+            <ion-col>{{ crypto.price_usd }}</ion-col>
           </ion-row>
         </ion-grid>
       </div>
@@ -32,27 +36,73 @@
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios, { AxiosResponse } from "axios";
+
+// Definiskan interface struktur untuk cryptoData
+interface CryptoData {
+  id: string;
+  name: string;
+  symbol: string;
+  price_usd: string;
+}
+
+// Definisikan interface untuk object response CryptoData
+interface ResponseCryptoData {
+  data: CryptoData[];
+}
+
 export default defineComponent({
+  name: "HomePage",
+  data() {
+    return {
+      cryptoData: [] as CryptoData[],
+    };
+  },
   methods: {
-    async getData(): Promise<void> {},
+    async getData(): Promise<void> {
+      try {
+        // Fetch data dengan axios
+        const response: AxiosResponse<ResponseCryptoData> = await axios.get(
+          "https://api.coinlore.net/api/tickers/"
+        );
+
+        this.cryptoData = response.data.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
 });
 </script>
 
 <style scoped>
+/* Container */
 .container {
-  background: white;
   padding: 10px 0px;
 }
+
+/* Button */
 .custom-button {
   color: white !important;
-}
-.custom-grid {
-  border: 2px solid #000;
 }
 .button-container {
   display: flex;
   justify-content: center;
   margin: 16px;
+}
+
+/* Table */
+.custom-grid {
+  border: 2px solid #000;
+  color: #000;
+  overflow: hidden;
+}
+.table-header {
+  font-weight: bold;
+  border-bottom: 2px solid #000;
+  padding: 10px;
+}
+.table-row {
+  padding: 10px;
 }
 </style>
